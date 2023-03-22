8f0d85b3a045ab99e85200f6a12ea3ead187f2ca
fedestritt3 <65463426+fedestritt3@users.noreply.github.com>
2023-03-22 14:49:47 +0100

    Update Flattener.kt

--- exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
+++ exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,18 @@
 object Flattener {
+    //every test must be runned setting the others not involved in @ignore
+    //this because the Flattener is a singleton object
+    private val flatList= mutableListOf<Int>()
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+
+       for(element in source){
+           when( element) {
+               is Int -> flatList.add(element)
+
+               is List<*> -> this.flatten(element)
+           }
+
+       }
+        return flatList.toList()
+
     }
 }
