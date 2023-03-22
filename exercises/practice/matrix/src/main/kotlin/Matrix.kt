17000554b6bd79ce6da87b1a52159042552ac66e
fedestritt3 <notarangelo.federico38@gmail.com>
2023-03-22 12:21:36 +0100

    es1

--- exercises/practice/matrix/src/main/kotlin/Matrix.kt
+++ exercises/practice/matrix/src/main/kotlin/Matrix.kt
@@ -1,17 +1,22 @@
 class Matrix(matrixAsString: String) {
 
-    private lateinit var matrix: List<MutableList<Int>>
+    private  var matrix: List<List<Int>>
 
     init {
         /*
         after the construction the sequence is immediately processed
         to obtain the matrix
          */
+        val tempMatrix = mutableListOf<MutableList<Int>>()
         matrixAsString.split("\n").forEachIndexed { index, s ->
+
+            tempMatrix.add(mutableListOf())
             s.split(" ").forEach {
-                matrix[index].add(it.toInt())
+                tempMatrix[index].add(it.toInt())
             }
         }
+        matrix= tempMatrix.toList()
+
 
 
     }
@@ -19,18 +24,20 @@ class Matrix(matrixAsString: String) {
 
     fun column(colNr: Int): List<Int> {
         val col = mutableListOf<Int>()
-        matrix.forEachIndexed { index, ints ->
-            if (index == colNr) {
-                col.add(ints[index])
+        matrix.forEach {
+            it.forEachIndexed { index, i ->
+                if(index == colNr-1){
+                    col.add(i)
+                }
             }
         }
-
         return col.toList()
+        
     }
 
 
     fun row(rowNr: Int): List<Int> {
-        return matrix[rowNr]
+        return matrix[rowNr-1]
     }
 }
 
