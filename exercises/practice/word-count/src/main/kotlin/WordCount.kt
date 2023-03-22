aa5c3aadf0d7968205d83a0d3c5c33a4c6451fa5
fedestritt3 <notarangelo.federico38@gmail.com>
2023-03-22 18:25:40 +0100

    Lab1 Es3 DONE. All test passed

--- exercises/practice/word-count/src/main/kotlin/WordCount.kt
+++ exercises/practice/word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,43 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+        val frequencyMap = mutableMapOf<String,Int>()
+        val wordList=phrase.lowercase()
+            .trim()
+            .replace("[!\"#$%&()*+,-./:;<=>?@\\[\\]^_{|}~]".toRegex(), " ")
+            .trim()
+            .split("\\s+".toRegex())
+
+
+
+        for(s in wordList){
+
+            var count= frequencyMap[s]
+            if( count ==null ){
+                count =0
+            }
+            frequencyMap[removePrefixesAndSuffixes(s)]=count + 1
+        }
+
+        return frequencyMap.toMap()
+
+    }
+
+    private fun removePrefixesAndSuffixes(s: String): String{
+        var formattedString= removePrefixes(s)
+        formattedString= removeSuffixes((formattedString))
+        return formattedString
+    }
+
+    private fun removePrefixes(s: String) :String{
+        if(s.startsWith("'"))
+            return s.removePrefix("'")
+        return s
+    }
+
+    private fun removeSuffixes(s: String) :String{
+        if(s.endsWith("'"))
+            return s.removeSuffix("'")
+        return s
     }
 }
