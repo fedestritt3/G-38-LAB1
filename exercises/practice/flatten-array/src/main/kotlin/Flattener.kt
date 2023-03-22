135b66a86efa99324e73c73de9b5e12a2e164f31
fedestritt3 <notarangelo.federico38@gmail.com>
2023-03-22 19:06:52 +0100

    modified flatten function to run all the GRADLE tests simultaneously

--- exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
+++ exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,20 +1,31 @@
 object Flattener {
-    /*every test must be runned setting the others not involved in @ignore
-    this because the Flattener is a singleton object*/
+    /*
+    I used this schema because a recursive function require a global element in which store the value
 
 
-    private val flatList= mutableListOf<Int>()
+    So is possible to use the flatten method as a recursive method by using as a Singleton object the flat list
+    (But using the technique just described is impossible to run all the test at the same time without an @Ignere annotation
+     so I preferred the below schema that allow to run all the test simultaneously)
+
+
+    */
+
     fun flatten(source: Collection<Any?>): List<Any> {
+        val flatList= mutableListOf<Int>()
 
-       for(element in source){
-           when( element) {
-               is Int -> flatList.add(element)
+        return recursiveFlat(source, flatList)
 
-               is List<*> -> this.flatten(element)
-           }
 
-       }
+    }
+    private fun recursiveFlat(source: Collection<Any?>, flatList: MutableList<Int>) : List<Any>{
+        for(element in source){
+            when( element) {
+                is Int -> flatList.add(element)
+
+                is List<*> -> this.recursiveFlat(element,flatList)
+            }
+
+        }
         return flatList.toList()
-
     }
 }
